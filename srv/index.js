fee32253c2d60cefa7770586318cda963aaa4da5
stormiix <madadj4@gmail.com>
2020-03-21 01:14:51 +0100

    feat: add name and date to output file

--- srv/index.js
+++ srv/index.js
@@ -19,9 +19,11 @@ export default (app, http) => {
       const month = ('0' + (currentDate.getMonth() + 1)).slice(-2)
       const day = ('0' + currentDate.getDate()).slice(-2)
       const filledFields = {
-        'Champ de texte 2.Page 1': `${dt.getDate()}-${dt.getMonth() +
-          1}-${dt.getFullYear()}`,
-        'Champ de texte 1.Page 1': name + '\r\n',
+        'Champ de texte 2.Page 1': `${('0' + dt.getDate()).slice(-2)}-${(
+          '0' +
+          (dt.getMonth() + 1)
+        ).slice(-2)}-${dt.getFullYear()}`,
+        'Champ de texte 1.Page 1': name,
         'Champ de texte 3.Page 1': address
         // 'Champ de texte 4.Page 1': city
         // 'Champ de texte 5.Page 1': day + '\r\n',
@@ -38,38 +40,72 @@ export default (app, http) => {
         save: 'pdf'
       })
       fs.writeFileSync(unsignedPDF.name, pdf)
-
-      // Save image
-      const base64Data = signature.replace(/^data:image\/png;base64,/, '')
-      fs.writeFileSync(signatureFile.name, base64Data, 'base64', err => {
-        console.error(err)
-      })
+      if (signature) {
+        // Save image
+        const base64Data = signature.replace(/^data:image\/png;base64,/, '')
+        fs.writeFileSync(signatureFile.name, base64Data, 'base64', err => {
+          console.error(err)
+        })
+      }
 
       // Sign the PDF
       const pdfDoc = new HummusRecipe(unsignedPDF.name, signedPDF.name)
       const width = pdfDoc.metadata['1'].width
       const height = pdfDoc.metadata['1'].height
-      pdfDoc
-        .editPage(1)
-        .text(city, width - 220, height - 150, { color: '#000000', bold: true })
-        .text(day, width - 120, height - 150, { color: '#000000' })
-        .text(month, width - 95, height - 150, { color: '#000000' })
-        .image(signatureFile.name, width - 150, height - 100, {
-          height: 45,
-          keepAspectRatio: true
-        })
-        .endPage()
-        .endPDF()
+
+      // TODO: refactor the following spagetti code
+      if (signature) {
+        pdfDoc
+          .editPage(1)
+          .text(city, width - 220, height - 150, {
+            color: '#000000',
+            bold: true
+          })
+          .text(day, width - 120, height - 150, { color: '#000000' })
+          .text(month, width - 95, height - 150, { color: '#000000' })
+          .image(signatureFile.name, width - 150, height - 100, {
+            height: 45,
+            keepAspectRatio: true
+          })
+          .endPage()
+          .endPDF()
+      } else {
+        pdfDoc
+          .editPage(1)
+          .text(city, width - 220, height - 150, {
+            color: '#000000',
+            bold: true
+          })
+          .text(day, width - 120, height - 150, { color: '#000000' })
+          .text(month, width - 95, height - 150, { color: '#000000' })
+          .endPage()
+          .endPDF()
+      }
+
       signatureFile.removeCallback()
       unsignedPDF.removeCallback()
-      client.emit('preview', Buffer.from(signedPDF.name).toString('base64'))
+      client.emit(
+        'preview',
+        Buffer.from(`${signedPDF.name}|${name}`).toString('base64')
+      )
     })
   })
   app.get('/preview/:file', (req, res) => {
-    const filePath = Buffer.from(req.params.file, 'base64').toString('ascii')
+    const payload = Buffer.from(req.params.file, 'base64')
+      .toString('ascii')
+      .split('|')
+    const filePath = payload[0]
+    const name = payload[1]
+    const currentDate = new Date()
+    const month = ('0' + (currentDate.getMonth() + 1)).slice(-2)
+    const day = ('0' + currentDate.getDate()).slice(-2)
+    const year = currentDate.getFullYear()
     if (filePath) {
       const readStream = fs.createReadStream(filePath)
-      const filename = encodeURIComponent('Attestation.pdf')
+      // TODO: remove name or sanitize it somehow ?
+      const filename = encodeURIComponent(
+        `Attestation ${name} ${day}-${month}-${year}.pdf`
+      )
 
       readStream.on('open', function() {
         res.setHeader(
